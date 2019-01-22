cfb545926925ec1130828156a8b04840461b9a0d
juanMa <juanma@gmail.com>
2019-01-22 16:05:27 -0500

    usuario2

---
+++ src/com/aplicacion/java/valueobject/UsuarioVO2.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.aplicacion.java.valueobject;
+
+/**
+ *
+ * @author Administrador
+ */
+public class UsuarioVO2 {
+    
+    private int idPersona;
+    private int id;
+    private String nombre;
+    private String contra;
+    private String permisos;
+
+    public UsuarioVO2(int idPersona, int id, String nombre, String contra, String permisos) {
+        this.idPersona = idPersona;
+        this.id = id;
+        this.nombre = nombre;
+        this.contra = contra;
+        this.permisos = permisos;
+    }
+
+    public String getPermisos() {
+        return permisos;
+    }
+
+    public void setPermisos(String permisos) {
+        this.permisos = permisos;
+    }
+
+    public int getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(int idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContra() {
+        return contra;
+    }
+
+    public void setContra(String contra) {
+        this.contra = contra;
+    }
+    
+    
+    
+    
+}
